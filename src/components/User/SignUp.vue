bce56a48e37c2afad6391353f624ff6f7e9d9272
xrr2016 <xiaoranran1993@163.com>
2017-08-08 22:10:52 +0800

    :fire:

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,11 +1,68 @@
 <template>
-  <div>
-    <p>The SignUp page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="email" label="mail" id="email" v-model="email"
+                      type="email" required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="password" label="password" id="password" v-model="password"
+                      type="password" required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="confirmPassword" label="Comfirm Password"        id="comfirmPassword" v-model="confirmPassword" type="password"
+                    :rules="[comparePassword]">
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit" @click="handleSubmit($event)">注册</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePassword () {
+      return this.password === this.confirmPassword ? '' : '密码不相等。'
+    }
+  },
+  methods: {
+    handleSubmit (event) {
+      event.preventDefault()
+      console.log({
+        email: this.email,
+        password: this.password,
+        confirm: this.confirmPassword
+      })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import App from './App'
 import router from './router'
-import { store } from './store'
+import {
+  store
+} from './store'
 import dateFilter from './filters/date'
 
 Vue.use(Vuetify)
@@ -15,5 +18,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCOo6SINl-cltqm1fFif8KPGU4aYAdcb8I',
+      authDomain: 'share-f0f61.firebaseapp.com',
+      databaseURL: 'https://share-f0f61.firebaseio.com',
+      projectId: 'share-f0f61',
+      storageBucket: 'share-f0f61.appspot.com'
+    })
+  }
 })
